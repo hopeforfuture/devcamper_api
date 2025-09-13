78628e9c3967c24a56f177b2c040d9f4b42f04b9
hopeforfuture <mnbl87@gmail.com>
2025-09-13 13:38:07 +0530

    User signup and login is also completed.

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -29,6 +29,13 @@ const BootcampSchema = new mongoose.Schema(
       type: String,
       maxlength: [20, "Phone number can not be longer than 20 characters"],
     },
+    email: {
+      type: String,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+    },
     address: {
       type: String,
       required: [true, "Please add an address"],
